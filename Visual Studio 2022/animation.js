6a583ca83a7d24d56af535d3a964bf3b07e66678
ELVISTEC12 <126284708+ELVISTEC12@users.noreply.github.com>
2023-09-24 16:30:15 -0300

    Add files via upload

---
+++ Visual Studio 2022/animation.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', function () {
+    const element = document.querySelector('.scroll-element');
+    if (isInViewport(element)) {
+        element.classList.add('show');
+    }
+});
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
